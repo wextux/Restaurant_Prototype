51c33804344edf29bb0acab5cab2f5f44a75b7ec
Will Fairclough <willbf6@gmail.com>
2011-09-15 18:39:15 -0400

    Updated Application Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def title
+    @basetitle = "Restaurant Prototype"
+    if @title.nil?
+      @basetitle
+    else
+      "#{@basetitle} | #{@title}"
+    end
+  end
+  
+  def logo
+    image_tag("logo.png", :alt => "Restaurant Icon", :class => "round")
+  end
+
 end
