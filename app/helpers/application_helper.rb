3907d8c77863dc09b9c25449b186c367241fafe8
Will Fairclough <willbf6@gmail.com>
2011-09-18 22:29:18 -0400

    Switched stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,13 @@ module ApplicationHelper
   def logo
     image_tag("logo.png", :alt => "Restaurant Icon", :class => "round")
   end
+  
+  def active_page(active_page)
+    if current_page?(active_page)
+      return "active"
+    else
+      return nil
+    end
+  end
 
 end
