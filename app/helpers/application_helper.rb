a93d0a8b0de1902a1860bd049b212217daf7cdc9
Will Fairclough <willbf6@gmail.com>
2011-09-19 17:52:06 -0400

    Fixed menu issue when on a page that doesnt include the menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,14 @@ module ApplicationHelper
     image_tag("logo.png", :alt => "Restaurant Icon", :class => "round")
   end
   
-  def active_page(active_page)
-    if current_page?(active_page)
+  def active_page(active_path)
+    if current_page?(root_path)
+      if current_page?(active_path)
+        return "active"
+      end
+    end
+    
+    if request.path.include? active_path
       return "active"
     else
       return nil
