469f0066ba23a0ab9082430a7aebdb5fff78b61c
Will Fairclough <willbf6@gmail.com>
2011-09-24 11:18:51 -0400

    fixed bug with show action for menu_devices

--- app/controllers/menu_devices_controller.rb
+++ app/controllers/menu_devices_controller.rb
@@ -25,6 +25,12 @@ class MenuDevicesController < ApplicationController
   end
   
   def create
+    
+    configatron.apn.passphrase  = '' 
+    configatron.apn.port = 2195 
+    configatron.apn.host  = 'gateway.sandbox.push.apple.com' 
+    configatron.apn.cert = File.join(Rails.root, 'config', 'apple_push_notification_development.pem')
+    
     @notification = APN::Notification.new 
     @notification.device_id = params[:d]
     @notification.badge = params[:b]
